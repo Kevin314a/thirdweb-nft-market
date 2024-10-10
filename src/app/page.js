bbc8eb80e2eb675600888e9c571a959aa8dc3e55
ADNAN NIAZI <65025349+adnanniazi@users.noreply.github.com>
2024-10-10 18:42:41 +0500

    updated index.js

--- src/app/page.js
+++ src/app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
 
   return (
     <>
-      <section className="lg:pt-20 pt-24 relative z-10">
-        <div className="max-w-[1920px] lg:px-[50px] px-5 mx-auto">
+      <section className="lg:pt-20 pt-24 relative z-20">
+        <div className="max-w-[1920px] px-6 mx-auto">
           <h2 className="text-[15px] sm:mb-3 font-semibold text-white leading-normal sm:text-start text-center mb-6">
             Featured
             <span className="bg-text-bg  bg-clip-text text-transparent">
@@ -39,7 +39,7 @@ export default function Home() {
       </section>
 
       <section className="pt-10 relative z-10">
-        <div className="max-w-[1920px] lg:px-[50px] px-5 mx-auto">
+        <div className="max-w-[1920px] px-6 mx-auto relative z-10">
           <h2 className="text-[15px] sm:mb-3 font-semibold text-white leading-normal sm:text-start text-center mb-10">
             Token
             <span className="bg-text-bg  bg-clip-text text-transparent">
@@ -48,7 +48,7 @@ export default function Home() {
             </span>
           </h2>
           <div className="flex lg:flex-nowrap flex-wrap justify-center gap-6">
-            <div className="lg:w-[38%] md:w-1/2 w-full">
+            <div className="">
               <Image
                 width={500}
                 height={350}
@@ -58,7 +58,7 @@ export default function Home() {
                 priority
               />
             </div>
-            <div className="lg:w-[62%] md:w-1/2 w-full">
+            <div className="">
               <Image
                 width={815}
                 height={350}
@@ -68,6 +68,17 @@ export default function Home() {
                 priority
               />
             </div>
+            <div className="">
+              <Image
+                width={500}
+                height={350}
+                src="/images/funia-img.png"
+                className="rounded-[15px] w-full"
+                alt=""
+                priority
+              />
+            </div>
+            
           </div>
         </div>
         <div className="absolute bottom-0 left-0 max-h-[312px] -z-10">
@@ -77,19 +88,21 @@ export default function Home() {
             alt=""
           />
         </div>
+        <div className="h-[653.31px] w-[645.18px] bg-[#200B07] blur-[250px] absolute -top-[50%] -left-44 -z-20"></div>
+
       </section>
 
       <section className="md:pt-4 pt-10 pb-[34px] relative">
-        <div className="max-w-[1920px] lg:px-[50px] px-5 mx-auto z-10 relative">
+        <div className="max-w-[1920px] px-6 mx-auto z-10 relative">
           <div className="w-full flex items-center justify-between">
           <TableTabs/>
           </div>
         </div>
-        <div className="h-[900px] bg-[#1D0B07] blur-[200px] absolute w-1/4 -top-2/3 right-0 z-0"></div>
+        <div className="h-[653.31px] w-[645.18px] bg-[#200B07] blur-[250px] absolute -top-[35%] -right-44 z-0"></div>
       </section>
 
       <section className="pt-10 relative">
-        <div className="max-w-[1920px] lg:px-[50px] px-5 mx-auto relative z-10">
+        <div className="max-w-[1920px] px-6 mx-auto relative z-10">
           <div className="mb-14">
             <h2 className="text-[15px] sm:mb-3 font-semibold text-white leading-normal sm:text-start text-center mb-10">
               Launchpad NFT
@@ -129,6 +142,8 @@ export default function Home() {
           />
         </div>
 
+
+
         <div className="absolute -translate-y-1/2 top-1/2 -mt-36 left-0">
           <img
             className="max-h-[312px] bg-contain w-auto"
@@ -136,6 +151,10 @@ export default function Home() {
             alt=""
           />
         </div>
+
+        <div className="h-[237px] w-[237px] bg-[#200B07] blur-[250px] absolute -top-[0%] -left-10 z-0"></div>
+        <div className="h-[555px] w-[547px] bg-[#200B07] blur-[250px] absolute -bottom-[0%] -right-44 z-0"></div>
+
 
         <div className="absolute -bottom-34 right-0">
           <img
@@ -149,7 +168,7 @@ export default function Home() {
       </section>
 
       <section className="pt-10 relative">
-        <div className="max-w-[1920px] lg:px-[50px] px-5 mx-auto relative z-10">
+        <div className="max-w-[1920px] px-6 mx-auto relative z-10">
           <h2 className="text-[15px] sm:mb-3 font-semibold text-white leading-normal sm:text-start text-center mb-6">
             Memecoins on
             <span className="bg-text-bg  bg-clip-text text-transparent">
@@ -168,13 +187,13 @@ export default function Home() {
           </div>
           <MemecoinsSlider/>
 
-          <div className="grid md:grid-cols-2 grid-cols-1 mb-16 mt-[70px] md:gap-12 gap-5">
+          <div className="grid md:grid-cols-2 grid-cols-1 mb-16 mt-[70px] md:gap-12 gap-5 max-w-[1440px] mx-auto">
             <div className="relative">
               <Image
                width={646}
                height={291}
                 src="/images/marketplace-img.png"
-                className="w-full h-full rounded-[16px]"
+                className="!w-full h-full rounded-[16px]"
                 alt=""
               />
               <div className="absolute bottom-[18px] left-5 max-w-[150px] w-full">
@@ -190,8 +209,8 @@ export default function Home() {
               <Image
                 width={646}
                 height={291}
-              src="/images/dex-img.png" className="rounded-[16px]" alt="" />
-              <div className="absolute w-full h-auto bottom-[18px] left-5 max-w-[150px]">
+              src="/images/dex-img.png" className="w-full rounded-[16px]" alt="" />
+              <div className="absolute h-auto bottom-[18px] left-5 max-w-[150px]">
                 <a
                   href="#"
                   className="text-base transition-all justify-center ease-out duration-500 hover:bg-golden-1100 max-w-[150px] w-full text-center mx-auto font-medium text-white flex items-center rounded-lg bg-golden-1000 border border-golden-1000 p-[7px] px-[14px] gap-2"
@@ -205,7 +224,7 @@ export default function Home() {
             <Image
               width={1340}
               height={402}
-            src="/images/banner.png" className="w-full sm:min-h-[250px] min-h-[150px] object-cover h-auto rounded-[16px]" alt="" />
+            src="/images/banner.png" className="w-full sm:min-h-[250px] min-h-[200px] object-cover h-auto rounded-[16px]" alt="" />
             <div className="absolute top-[22px] left-5 max-w-[150px] w-full">
               <a
                 href="#"
@@ -223,6 +242,9 @@ export default function Home() {
             alt=""
           /> 
         </div>
+
+        <div className="h-[555px] w-[547px] bg-[#200B07] blur-[250px] absolute -top-[0%] -left-44 z-0"></div>
+        <div className="h-[966px] w-[952px] bg-[#200B07] blur-[250px] absolute -bottom-[0%] -right-44 z-0"></div>
 
         <div className="absolute -bottom-20 right-0 z-0">
         <img
